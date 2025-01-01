f47372f2ba71c13ad041e6b361e552f9772aa118
Vikash Prithiani <vikash.pirthiani@gmail.com>
2025-01-01 20:46:56 +0100

    template rendering

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,10 @@
+from flask import render_template
 from app import app 
+
 
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return 'Hello World'
+    user = {'username': 'Vikash'}
+    return render_template('index.html', user=user)
